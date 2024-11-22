ce0f218c6395743719f37697b96294895aafa917
phmthaj <thai.pq235558@sis.hust.edu.vn>
2024-11-22 23:39:05 +0700

    Add files via upload

---
+++ Lab02/OtherProjects/src/hust/soict/dsai/garbage/NoGarbage.java
@@ -0,0 +1,13 @@
+package hust.soict.dsai.garbage;
+
+public class NoGarbage {
+    public static void main(String[] args) {
+        StringBuilder reusableStringBuilder = new StringBuilder(); // Reusable StringBuilder
+        while (true) {
+            reusableStringBuilder.setLength(0);
+            // Generate some data using the StringBuilder
+            reusableStringBuilder.append("Some data");
+            // Continue processing without creating additional garbage
+        }
+    }
+}
