c0b2dc0b52639db05e5760880a742fce51c2f752
phmthaj <thai.pq235558@sis.hust.edu.vn>
2024-11-29 17:44:18 +0700

    Delete Lab02 directory

--- Lab02/OtherProjects/src/hust/soict/dsai/garbage/GarbageCreator.java
+++
@@ -1,9 +0,0 @@
-package hust.soict.dsai.garbage;
-
-public class GarbageCreator {
-    public static void main(String[] args) {
-        while (true) {
-            String garbage = new String(new char[1000000]); // Creating a large string
-        }
-    }
-}
